d9e8bf196bfc622e7392baeb722f4e9c6f749398
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-21 20:39:05 +0700

    ADD - Menambahkan fungsi untuk memperbarui pilihan Mata Kuliah dan Kelas pada state KRS

--- app/src/main/java/com/example/pam_pert8/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/pam_pert8/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -5,8 +5,21 @@ import com.example.pam_pert8.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel : ViewModel() {
     private val _krsState = MutableStateFlow(RencanaStudi())
     val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+
+    fun setMataKuliah(mkPilihan: String){
+        _krsState.update {
+            stateMK -> stateMK.copy(namaMk = mkPilihan)
+        }
+    }
+
+    fun setKelas(kelasPilihan: String){
+        _krsState.update {
+                stateKelas -> stateKelas.copy(kelasMk = kelasPilihan)
+        }
+    }
 }
