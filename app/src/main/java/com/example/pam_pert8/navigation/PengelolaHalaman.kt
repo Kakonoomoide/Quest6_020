fb91dceb15044e9dadc77ec82cee46fa102e6431
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-21 21:38:10 +0700

    ADD - Menambahkan fungsi navigasi untuk pengelolaan halaman menggunakan Jetpack Compose

--- app/src/main/java/com/example/pam_pert8/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/pam_pert8/navigation/PengelolaHalaman.kt
@@ -1,5 +1,19 @@
 package com.example.pam_pert8.navigation
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.pam_pert8.ui.view.screen.MahasiswaFormView
+import com.example.pam_pert8.ui.view.screen.SplashView
+import com.example.pam_pert8.ui.view.viewmodel.MahasiswaViewModel
+import com.example.pam_pert8.ui.view.viewmodel.RencanaStudyViewModel
+
 enum class Halaman{
     Splash,
     InputMahasiswa,
@@ -7,3 +21,26 @@ enum class Halaman{
     TampilData
 }
 
+@Composable
+fun PengelolaanHalaman(
+    modifier: Modifier = Modifier,
+    mahasiswaViewModel: MahasiswaViewModel = viewModel(),
+    krsViewModel: RencanaStudyViewModel = viewModel(),
+    navController: NavHostController = rememberNavController()
+){
+    val mahasiswaUiState = mahasiswaViewModel.mhsStateUi.collectAsState().value
+    NavHost(
+        navController = navController,
+        startDestination = Halaman.Splash.name,
+        modifier = Modifier.padding()
+    ) {
+        composable(route = Halaman.Splash.name){
+            SplashView (onMulaiButton ={
+                navController.navigate(
+                    Halaman.InputMahasiswa.name
+                )
+            })
+        }
+
+    }
+}
