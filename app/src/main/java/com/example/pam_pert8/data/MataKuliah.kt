1d947c439e98985798e6efdb1a14903f6ba76a6c
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-21 12:01:34 +0700

    ADD - menambahkan data opstion di MataKuliah

--- app/src/main/java/com/example/pam_pert8/data/MataKuliah.kt
+++ app/src/main/java/com/example/pam_pert8/data/MataKuliah.kt
@@ -1,5 +1,12 @@
 package com.example.pam_pert8.data
 
 object MataKuliah {
-
+    val options = listOf(
+        "Pemograman Aplikasi Mobile Lanjut",
+        "Pemograman Aplikasi web Framework",
+        "Prototyping",
+        "Aplikasi Multimedia",
+        "Kecerdasan Buatan",
+        "Computer Vision",
+    )
 }
