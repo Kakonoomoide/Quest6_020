7fd9a7f8e03abb4958f905d71a4f6a0bf82dde8b
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-22 09:52:36 +0700

    ADD - menambahkan function untuk menampilkan data

--- app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
@@ -101,7 +102,37 @@ fun DetailView(
                 )
                 .fillMaxSize(),
         ){
+            Column (
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            )
+            {
+                inputDataMhs.forEach{ data ->
 
+
+                }
+
+            }
         }
+    }
+}
+
+@Composable
+fun DetailMhs(
+    judul: String, isinya: String
+){
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp)
+    ){
+        Text(text = judul,
+            modifier = Modifier.weight(0.8f))
+        Text(text = " : ",
+            modifier = Modifier.weight(0.2f))
+        Text(text = isinya,
+            modifier = Modifier.weight(2f))
     }
 }
