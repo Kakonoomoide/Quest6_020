c8aa79a3aeb500c9cd2eb4e214d9bbc07e9c0814
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-22 09:36:02 +0700

    ADD - menambahkan import data dari data model

--- app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
@@ -1,8 +1,15 @@
 package com.example.pam_pert8.ui.view.screen
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.pam_pert8.model.Mahasiswa
+import com.example.pam_pert8.model.RencanaStudi
 
 @Composable
-fun DetailView(){
-
+fun DetailView(
+    modifier: Modifier = Modifier,
+    dataDiriMhs : Mahasiswa,
+    dataMkMhs : RencanaStudi,
+    onBackClicked : () -> Unit
+){
 }
