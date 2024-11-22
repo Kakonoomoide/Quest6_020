325ca17795edd1d7e273613489c24280e54e16f6
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-22 09:41:31 +0700

    ADD - menambahkan komponen column

--- app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
@@ -1,7 +1,13 @@
 package com.example.pam_pert8.ui.view.screen
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import com.example.pam_pert8.R
 import com.example.pam_pert8.model.Mahasiswa
 import com.example.pam_pert8.model.RencanaStudi
 
@@ -19,4 +25,17 @@ fun DetailView(
         Pair("Matakuliah", dataMkMhs.namaMk),
         Pair("Kelas", dataMkMhs.kelasMk)
     )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                color = colorResource(
+                    id = R.color.primary
+                )
+            ),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
 }
