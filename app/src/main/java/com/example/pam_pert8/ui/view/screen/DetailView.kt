5fdb9419e3234e7ab3a750fb5930e2ccfb9ec9bc
Kakonoomoide <rizkiramada128@gmail.com>
2024-11-22 09:44:06 +0700

    ADD - menambahkan logo dan text header

--- app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
+++ app/src/main/java/com/example/pam_pert8/ui/view/screen/DetailView.kt
@@ -1,12 +1,26 @@
 package com.example.pam_pert8.ui.view.screen
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pam_pert8.R
 import com.example.pam_pert8.model.Mahasiswa
 import com.example.pam_pert8.model.RencanaStudi
@@ -36,6 +50,32 @@ fun DetailView(
             ),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-
+        Spacer(modifier = Modifier.padding(16.dp))
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Image(
+                painter = painterResource(
+                    id = R.drawable.logoumy
+                ),
+                contentDescription = "",
+                contentScale = ContentScale.Crop,
+                modifier = Modifier.size(45.dp).clip(CircleShape)
+            )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column {
+                Text(
+                    text = "Universitas Muhammadiyah Yogyakarta",
+                    color = Color.Red,
+                    fontSize = 15.sp,
+                    fontWeight = FontWeight.Bold
+                )
+                Text(
+                    text = "Unggul dan Islami",
+                    color = Color.Red,
+                    fontWeight = FontWeight.Light
+                )
+            }
+        }
     }
 }
